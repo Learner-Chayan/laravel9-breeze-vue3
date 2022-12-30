6953ffea558975a8e2be54566051a35a8cd97269
chayan roy <chayanroycmt50@gmail.com>
2022-12-30 09:46:39 +0600

    delete chirp done

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,8 @@ Route::middleware('auth')->group(function () {
 
 Route::resource('chirps',ChirpController::class)
     //->only(['index','store'])
-    ->only(['index','store','update'])
+    //->only(['index','store','update'])
+    ->only(['index','store','update','destroy'])
     ->middleware(['auth','verified']);
 
 require __DIR__.'/auth.php';
